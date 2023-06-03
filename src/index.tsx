0cb0547045ea0504304c139761c2ba11515a5a66
Jordan Cuker <jordan.cuker@gmail.com>
2023-06-02 23:33:54 -0500

    deprecate and deploy old

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/App';
-import * as serviceWorker from './serviceWorker';
+
+function App() {
+   return (
+      <div >
+         🚧Old website not maintained.🚧
+         <br/>
+         Senior software engineer, loves typescript and solving hard problems.
+         <br/>
+      </div>
+   );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
